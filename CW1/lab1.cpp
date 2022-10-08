678b0a757ca374dec15b30040e6ac6ec541e402b
s23215 <s23215@pjwstk.edu.pl>
2022-10-08 16:53:39 +0200

    lab1

---
+++ CW1/lab1.cpp
@@ -0,0 +1,46 @@
+#include <any>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using mojamapa_t = std::map<std::string, int>;
+using moja_funkcja = std::function<double (mojamapa_t)>;
+
+void wypisz(mojamapa_t mapa, moja_funkcja fun) {
+    using namespace std;
+    cout << fun(mapa) << endl;
+}
+
+double convert_to_degrees(double x) {
+    return x * 3.14159/180;
+}
+
+int main(int argc, char **argv) {
+    using namespace std;
+    vector<string> argumenty(argv, argv + argc);
+    map<string, int> mapa;
+    map<string, moja_funkcja> formatery;
+
+    for (int i = 2; i < argumenty.size(); i++) {
+        mapa.insert({{"arg" + to_string(i-1), stoi(argumenty.at(i))}});
+    }
+
+    formatery["add"] = [](map<string, int> map) { return map.at("arg1") + map.at("arg2"); };
+    formatery["mod"] = [](map<string, int> map) { return map.at("arg1") %  map.at("arg2"); };
+    formatery["sin"] = [](map<string, int> map) {
+        double x = convert_to_degrees(map.at("arg1")*1.0);
+        return sin(x);
+    };
+
+    try{
+        auto selected_f = argumenty.at(1);
+        wypisz(mapa, formatery.at(selected_f));
+    } catch (std::out_of_range aor) {
+        cout << "Jestem kodem Krystiana" << endl;
+        cout << "Zostalem zrobiony na zajecia do Pana Puzniakowskiego";
+    }
+    return 0;
+}
